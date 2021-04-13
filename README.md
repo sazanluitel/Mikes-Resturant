02fee90fdda982ec3672a95fe7648aa8e2bd1c63
sonangrai <shonahangrae@gmail.com>
2021-04-13 11:07:44 +0545

    initialised the project with gulstarter-bootstrap

---
+++ README.md
@@ -0,0 +1,31 @@
+## Gulp Starter
+
+This is project is created with a gulp starter for twig templating and includes the bootstrap 5.
+
+To begin first run
+
+```
+npm install
+```
+
+To run the engine. It will create the build folder with all the HTML files.
+
+```
+npm run serve
+```
+
+It is configured with the following.
+
+- gulp-twig2html (Convets all the twig files to HTML)
+- gulp-sass (To Compile the SASS/SCSS files)
+- postcss (The postcss middleware used to convert css which can be understood by most browsers)
+- autoprefixer (Used to add prefix of css as per the browsers)
+- browser-sync (Used to create a local server and test the development builds)
+- gulp
+- gulp-imagemin (To minify the images)
+- gulp-postcss (The postcss plugin for gulp)
+- gulp-rename (To remanme the twig file to HTML)
+- gulp-sass-glob
+- gulp-sourcemaps
+- gulp-uglify (Minify JS)
+- bootstrap5
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var sassGlob = require("gulp-sass-glob");
+var devserver = require("browser-sync");
+var twig = require("gulp-twig2html");
+var rename = require("gulp-rename");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+
+//Copying the twig to HTML
+gulp.task("twig", function () {
+  return gulp
+    .src(["src/**/*.twig", "!src/**/_*.twig"])
+    .pipe(twig({}))
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest("dist"));
+});
+
+//Optimizing Images
+gulp.task("imageMin", async () => {
+  gulp.src("src/img/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
+});
+
+//Minifying Js
+gulp.task("minifyJs", async () => {
+  gulp
+    .src(["node_modules/bootstrap/dist/js/bootstrap.js", "src/js/*.js"])
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+});
+
+//SCSS Compiling
+gulp.task("sass", async () => {
+  gulp
+    .src("src/sass/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(postcss([autoprefixer({ flexbox: true })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/css"));
+});
+
+//Default
+gulp.task(
+  "build",
+  gulp.series("sass", "minifyJs", "twig", "imageMin"),
+  async () => {
+    console.log("Ola Gulp is Walking...");
+  }
+);
+
+//Watching
+const watch = async () => {
+  gulp.watch("src/sass/**/*.scss", gulp.series("sass", "browser-reload"));
+  gulp.watch("src/js/*.js", gulp.series("minifyJs", "browser-reload"));
+  gulp.watch("src/**/*.twig", gulp.series("twig", "browser-reload"));
+  gulp.watch("src/img/*", gulp.series("imageMin", "browser-reload"));
+  httpserver.init(serveoptions);
+};
+
+//Devserver
+const serveoptions = {
+  server: {
+    baseDir: "./dist",
+    index: "index.html",
+    serveStaticOptions: {
+      extensions: ["html"],
+    },
+  },
+};
+const httpserver = devserver.create();
+
+gulp.task("browser-reload", function (cb) {
+  httpserver.reload();
+  cb();
+});
+
+module.exports.serve = gulp.series("build", watch);
