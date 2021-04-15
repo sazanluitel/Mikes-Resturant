123895754458d0136478a865f0ea35a356b533cb
sonangrai <shonahangrae@gmail.com>
2021-04-15 11:42:59 +0545

    menu page filter

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,19 @@
 
 (function () {
   console.log("Have a good day 🎈");
+
+  //Checkbox style toggle on check
+  const checkchecked = (a) => {
+    if (a.checked === true) {
+      a.classList.add("check");
+    } else {
+      a.classList.remove("check");
+    }
+  };
+  let allcheckbox = document.querySelectorAll(".filter");
+  allcheckbox.forEach((a) => {
+    a.addEventListener("click", () => {
+      checkchecked(a);
+    });
+  });
 })();
